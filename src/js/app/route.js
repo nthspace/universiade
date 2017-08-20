7a0b9c70ec83eddeea41314ebbcfce209b180cc2
Chia-Wei Li <dreamcwli@gmail.com>
2017-08-20 21:17:11 +0800

    Remove default margin of HTML body

--- src/js/app/route.js
+++ src/js/app/route.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import { Redirect, Route, Switch } from 'react-router-dom';
 
+import App from './components/App';
 import Root from './components/Root';
 import features from './features';
 
 export default () => (
-  <Switch>
-    <Route exact path="/" component={Root} />
-    {features.map(feature => (
-      <Route key={feature.constants.NAME} path={`/${feature.constants.NAME}`} component={feature.route} />
-    ))}
-    <Redirect to="/" />
-  </Switch>
+  <App>
+    <Switch>
+      <Route exact path="/" component={Root} />
+      {features.map(feature => (
+        <Route key={feature.constants.NAME} path={`/${feature.constants.NAME}`} component={feature.route} />
+      ))}
+      <Redirect to="/" />
+    </Switch>
+  </App>
 );
