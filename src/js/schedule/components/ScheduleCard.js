e306e2388c1b79ee533c8d6318fd04d051e76794
Chia-Wei Li <dreamcwli@gmail.com>
2017-08-22 21:40:56 +0800

    Update layout of schedule card

--- src/js/schedule/components/ScheduleCard.js
+++ src/js/schedule/components/ScheduleCard.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { Card, CardActions, CardHeader, CardTitle } from 'material-ui';
+import { Card, CardActions, CardHeader, CardTitle, RaisedButton } from 'material-ui';
 
-import TicketLink from './TicketLink';
 import { isScheduleActive, withDay } from '../utils';
 
-import LogoIcon from '../../../img/logo.png';
+import FemaleIcon from '../../../img/female.png';
+import MaleIcon from '../../../img/male.png';
+import MixedIcon from '../../../img/mixed.png';
 
 const propTypes = {
   schedule: PropTypes.shape({
@@ -21,21 +22,72 @@ const propTypes = {
 };
 const defaultProps = {};
 
+const styles = {
+  img: {
+    width: '4em',
+    height: '4em',
+    marginRight: '1em',
+  },
+  card: {
+    padding: '1em',
+    margin: '1em',
+  },
+  cardHeaderTitle: {
+    fontSize: '3em',
+  },
+  cardTitle: {
+    fontSize: '2em',
+  },
+  cardSubtitle: {
+    fontSize: '1.5em',
+  },
+  cardActions: {
+    textAlign: 'right',
+  },
+  button: {
+    width: '100%',
+    height: '5em',
+    maxWidth: '15em',
+    lineHeight: '5em',
+  },
+  buttonLabel: {
+    fontSize: '3em',
+  },
+};
+
+const Avatar = ({ gender }) => {
+  if (gender === '女') {
+    return <img style={styles.img} alt="female" src={FemaleIcon} />;
+  } else if (gender === '男') {
+    return <img style={styles.img} alt="male" src={MaleIcon} />;
+  }
+  return <img style={styles.img} alt="mixed" src={MixedIcon} />
+};
+
 const ScheduleCard = ({ schedule, available }) => (
-  <Card style={{ margin: '8px' }}>
+  <Card style={styles.card}>
     <CardHeader
-      title={withDay(schedule.date)}
-      subtitle={schedule.time}
-      avatar={LogoIcon}
+      titleStyle={styles.cardHeaderTitle}
+      title={`${withDay(schedule.date)}${schedule.time}`}
+      avatar={<Avatar gender={schedule.gender} />}
     />
     <CardTitle
+      titleStyle={styles.cardTitle}
+      subtitleStyle={styles.cardSubtitle}
       title={schedule.event}
       subtitle={schedule.place}
     />
     {isScheduleActive(schedule) && schedule.link
       ? (
-        <CardActions>
-          <TicketLink link={schedule.link} soldOut={!available} />
+        <CardActions style={styles.cardActions}>
+          <RaisedButton
+            style={styles.button}
+            labelStyle={styles.buttonLabel}
+            primary
+            disabled={!available}
+            label={available ? '購票' : '已售罄'}
+            href={schedule.link}
+          />
         </CardActions>
       )
       : null
